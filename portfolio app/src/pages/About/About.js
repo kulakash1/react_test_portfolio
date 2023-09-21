fba2e3444d19e1c9a800e532314a65e9a11e740a
Akash Kulshrestha <akashkulshrestha7@gmail.com>
2023-09-21 23:49:50 +0530

    Updated About Page

--- portfolio app/src/pages/About/About.js
+++ portfolio app/src/pages/About/About.js
@@ -8,6 +8,84 @@ import "bootstrap/dist/css/bootstrap.css";
 // import {Link} from 'react-router-dom';
 import Navbar1 from "../components/Navbar/navbar.js";
 
+
+
+
+// JS Code
+
+// JavaScript for filter functionality
+const filterInput = document.getElementById("filter");
+const skillsList = document.querySelector(".skills-list");
+
+filterInput.addEventListener("input", function () {
+  const searchTerm = filterInput.value.toLowerCase();
+  const skills = Array.from(skillsList.children);
+
+  skills.forEach((skill) => {
+    const skillName = skill.textContent.toLowerCase();
+    if (skillName.includes(searchTerm)) {
+      skill.style.display = "inline-block";
+    } else {
+      skill.style.display = "none";
+    }
+  });
+});
+
+// // To check
+// // JavaScript for smooth scrolling when clicking on navigation links
+// $(document).ready(function () {
+//   $("a").on("click", function (event) {
+//       if (this.hash !== "") {
+//           event.preventDefault();
+//           var hash = this.hash;
+//           $("html, body").animate(
+//               {
+//                   scrollTop: $(hash).offset().top,
+//               },
+//               800,
+//               function () {
+//                   window.location.hash = hash;
+//               }
+//           );
+//       }
+//   });
+// });
+
+// ... Existing JavaScript code ...
+
+// Function to display the contact popup
+function openContactPopup() {
+  var popup = document.getElementById("contact-popup");
+  popup.style.display = "block";
+}
+
+// Function to close the contact popup
+function closeContactPopup() {
+  var popup = document.getElementById("contact-popup");
+  popup.style.display = "none";
+}
+
+// Function to toggle visibility of the education and skills sections
+function toggleSectionVisibility(sectionId) {
+  var section = document.getElementById(sectionId);
+  if (section.style.display === "none") {
+      section.style.display = "block";
+  } else {
+      section.style.display = "none";
+  }
+}
+
+// ... Remaining JavaScript code ...
+
+
+
+
+
+
+
+
+
+
 const About = () => {
   return (
     <div className="">
@@ -80,7 +158,7 @@ const About = () => {
             <h2>Skills</h2>
             <div className="skills-filter">
               <label for="filter">Filter Skills:</label>
-              {/* <input type="text" id="filter" placeholder="Type to filter skills..."> */}
+              <input type="text" id="filter" placeholder="Type to filter skills..." />
             </div>
             <ul className="skills-list">
               <li>HTML</li>
