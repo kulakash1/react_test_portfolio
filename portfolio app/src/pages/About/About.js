60aaf76c0f58f532bc8b28e978476b688f97adaf
Akash Kulshrestha <akashkulshrestha7@gmail.com>
2023-09-22 00:11:47 +0530

    Updated Travel Page

--- portfolio app/src/pages/About/About.js
+++ portfolio app/src/pages/About/About.js
@@ -8,28 +8,25 @@ import "bootstrap/dist/css/bootstrap.css";
 // import {Link} from 'react-router-dom';
 import Navbar1 from "../components/Navbar/navbar.js";
 
+// // JS Code
 
+// // JavaScript for filter functionality
+// const filterInput = document.getElementById("filter");
+// const skillsList = document.querySelector(".skills-list");
 
+// filterInput.addEventListener("input", function () {
+//   const searchTerm = filterInput.value.toLowerCase();
+//   const skills = Array.from(skillsList.children);
 
-// JS Code
-
-// JavaScript for filter functionality
-const filterInput = document.getElementById("filter");
-const skillsList = document.querySelector(".skills-list");
-
-filterInput.addEventListener("input", function () {
-  const searchTerm = filterInput.value.toLowerCase();
-  const skills = Array.from(skillsList.children);
-
-  skills.forEach((skill) => {
-    const skillName = skill.textContent.toLowerCase();
-    if (skillName.includes(searchTerm)) {
-      skill.style.display = "inline-block";
-    } else {
-      skill.style.display = "none";
-    }
-  });
-});
+//   skills.forEach((skill) => {
+//     const skillName = skill.textContent.toLowerCase();
+//     if (skillName.includes(searchTerm)) {
+//       skill.style.display = "inline-block";
+//     } else {
+//       skill.style.display = "none";
+//     }
+//   });
+// });
 
 // // To check
 // // JavaScript for smooth scrolling when clicking on navigation links
@@ -53,38 +50,29 @@ filterInput.addEventListener("input", function () {
 
 // ... Existing JavaScript code ...
 
-// Function to display the contact popup
-function openContactPopup() {
-  var popup = document.getElementById("contact-popup");
-  popup.style.display = "block";
-}
+// // Function to display the contact popup
+// function openContactPopup() {
+//   var popup = document.getElementById("contact-popup");
+//   popup.style.display = "block";
+// }
 
-// Function to close the contact popup
-function closeContactPopup() {
-  var popup = document.getElementById("contact-popup");
-  popup.style.display = "none";
-}
+// // Function to close the contact popup
+// function closeContactPopup() {
+//   var popup = document.getElementById("contact-popup");
+//   popup.style.display = "none";
+// }
 
-// Function to toggle visibility of the education and skills sections
-function toggleSectionVisibility(sectionId) {
-  var section = document.getElementById(sectionId);
-  if (section.style.display === "none") {
-      section.style.display = "block";
-  } else {
-      section.style.display = "none";
-  }
-}
+// // Function to toggle visibility of the education and skills sections
+// function toggleSectionVisibility(sectionId) {
+//   var section = document.getElementById(sectionId);
+//   if (section.style.display === "none") {
+//     section.style.display = "block";
+//   } else {
+//     section.style.display = "none";
+//   }
+// }
 
 // ... Remaining JavaScript code ...
-
-
-
-
-
-
-
-
-
 
 const About = () => {
   return (
@@ -123,10 +111,13 @@ const About = () => {
             </div>
 
             <div className="col-12 col-md-6 order-1 order-md-1 d-none d-md-block">
-              <img src={require("../components/assets/image/Profile Pic/home/Profile Img.jpeg")}
+              <img
+                src={require("../components/assets/image/Profile Pic/home/Profile Img.jpeg")}
                 // src={require({link})}
                 // alt={imageName}
-                alt="Profile Pic" className="about-me-section-image" />
+                alt="Profile Pic"
+                className="about-me-section-image"
+              />
             </div>
           </div>
         </div>
@@ -158,7 +149,11 @@ const About = () => {
             <h2>Skills</h2>
             <div className="skills-filter">
               <label for="filter">Filter Skills:</label>
-              <input type="text" id="filter" placeholder="Type to filter skills..." />
+              <input
+                type="text"
+                id="filter"
+                placeholder="Type to filter skills..."
+              />
             </div>
             <ul className="skills-list">
               <li>HTML</li>
@@ -189,78 +184,3 @@ const About = () => {
 
 export default About;
 // export { About };
-
-
-
-
-
-
-
-
-
-
-// JS Code
-
-// // JavaScript for filter functionality
-// const filterInput = document.getElementById("filter");
-// const skillsList = document.querySelector(".skills-list");
-
-// filterInput.addEventListener("input", function () {
-//   const searchTerm = filterInput.value.toLowerCase();
-//   const skills = Array.from(skillsList.children);
-
-//   skills.forEach((skill) => {
-//     const skillName = skill.textContent.toLowerCase();
-//     if (skillName.includes(searchTerm)) {
-//       skill.style.display = "inline-block";
-//     } else {
-//       skill.style.display = "none";
-//     }
-//   });
-// });
-
-// // // To check
-// // // JavaScript for smooth scrolling when clicking on navigation links
-// // $(document).ready(function () {
-// //   $("a").on("click", function (event) {
-// //       if (this.hash !== "") {
-// //           event.preventDefault();
-// //           var hash = this.hash;
-// //           $("html, body").animate(
-// //               {
-// //                   scrollTop: $(hash).offset().top,
-// //               },
-// //               800,
-// //               function () {
-// //                   window.location.hash = hash;
-// //               }
-// //           );
-// //       }
-// //   });
-// // });
-
-// // ... Existing JavaScript code ...
-
-// // Function to display the contact popup
-// function openContactPopup() {
-//   var popup = document.getElementById("contact-popup");
-//   popup.style.display = "block";
-// }
-
-// // Function to close the contact popup
-// function closeContactPopup() {
-//   var popup = document.getElementById("contact-popup");
-//   popup.style.display = "none";
-// }
-
-// // Function to toggle visibility of the education and skills sections
-// function toggleSectionVisibility(sectionId) {
-//   var section = document.getElementById(sectionId);
-//   if (section.style.display === "none") {
-//       section.style.display = "block";
-//   } else {
-//       section.style.display = "none";
-//   }
-// }
-
-// // ... Remaining JavaScript code ...
